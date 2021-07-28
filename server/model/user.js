0086e2ec3aed659f4a9fb39a8afd00a20d754f58
Ritwik880 <rittiksinha12@gmail.com>
2021-07-28 18:48:10 +0530

    edited

--- server/model/user.js
+++ server/model/user.js
@@ -37,7 +37,7 @@ userSchema.pre('save', async function(next){
         this.password = await bcrypt.hash(this.password, 12);
         this.cpassword = await bcrypt.hash(this.cpassword, 12);
     }
-    next();
+    next()
 
 });
 userSchema.methods.generateAuthToken = async function(){
